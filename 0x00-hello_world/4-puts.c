0f4a4ae4f4f1b1ace5186cfa7e564aa299bf2ec6
Justin <tochukwunwanze5@gmail.com>
2022-12-08 08:02:05 -0800

    added a C programming file

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/** main- prints the sentence "Programming is like building a multilingual puzzle"
+ * not allowed to use printf must use puts
+ * return value 0
+ */
+int main () 
+{
+	puts("\"programming is like building a multilingual puzzle");
+	return (0);
+}
