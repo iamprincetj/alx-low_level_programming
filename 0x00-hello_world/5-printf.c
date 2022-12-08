f10d1b55994b69728379c058190317767a15ff8a
Justin <tochukwunwanze5@gmail.com>
2022-12-08 08:35:27 -0800

    added another C file

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - prints the phrase "with proper grammar,
+ * but the outcome is a piece of art"
+ * followed by a new line
+ * Must use printf not puts
+ * Return: the value 0
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
