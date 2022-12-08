4d250ced66fa4eca66cdfd6a8b820103b78ac612
Justin <tochukwunwanze5@gmail.com>
2022-12-08 12:19:00 -0800

    prints to the standard error

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - print exactly "and that piece of art is useful"
+ *  - Dora Korpar, 2015-10-19," to the standard eror.
+ * followed by a new line
+ *  Return: must have value 1
+ */
+
+int main(void)
+{
+	fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	return (1);
+}
