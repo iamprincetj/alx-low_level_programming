29171add97f7fa876f58bf538d1afbd4d58860ac
Justin <tochukwunwanze5@gmail.com>
2022-12-09 15:00:15 -0800

    9-print_comb.c alx project

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+*main - prints all possible combinations of single
+*digit numbers separated by ",", followed by a space
+*use only putchar function
+*Return: value 0 (success)
+*/
+
+int main(void)
+{
+	char a;
+	char b = ',';
+	char c = ' ';
+
+	for (a = 48; a < 58; a++)
+	{
+		putchar(a);
+		if (a != 57)
+		{
+			putchar(b);
+			putchar(c);
+		}
+	}
+	putchar('\n');
+	return (0);
+}
