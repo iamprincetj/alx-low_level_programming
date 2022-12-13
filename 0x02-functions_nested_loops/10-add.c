7ea5b5be2a0a8f607689f7c110ae45e5be3b2499
Justin <tochukwunwanze5@gmail.com>
2022-12-13 12:14:04 -0800

    10-add.c alx project

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+*add - add two intergers and return the result
+*@a: first number
+*@b: second number
+*Return: the result of the two numbers
+*/
+
+int add(int a, int b)
+{
+	int result = a + b;
+
+	return (result);
+}
