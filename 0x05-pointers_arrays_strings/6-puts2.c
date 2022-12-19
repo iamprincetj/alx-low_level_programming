1abb725ab293513ec37e604fe1afd8ae124d1b5e
Justin <tochukwunwanze5@gmail.com>
2022-12-19 07:56:59 -0800

    6-puts2.c alx project

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,20 @@
+#include"main.h"
+
+/**
+* puts2 - prints every character of a string
+*
+* @str: string parameter input
+*
+*/
+
+void puts2(char *str)
+{
+	int a;
+
+	for (a = 0; str[a] != '\0'; ++a)
+	{
+		if (a % 2 == 0)
+			_putchar(str[a]);
+	}
+	_putchar('\n');
+}
