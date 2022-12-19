d703df4e5a3d241bf3699c9f37a4afb1b244f482
Justin <tochukwunwanze5@gmail.com>
2022-12-19 05:35:32 -0800

    1-swap.c alx project

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+*swap_int - swaps the values of two integers.
+*@a: first number to swap
+*@b: second number to swap
+*/
+
+void swap_int(int *a, int *b)
+{
+	*a = 42;
+	*b = 98;
+}
