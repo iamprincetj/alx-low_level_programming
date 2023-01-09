c9725b17d18315f3167029cbdf2bbb4e8a950733
Justin <tochukwunwanze5@gmail.com>
2023-01-09 15:06:16 -0800

    1-strdup.c, alx project

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,32 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ *_strdup - returns a pointer to a newly allocated space in memory,
+ *which contains a copy of the string given as a parameter.
+ *@str: the string
+ *
+ *Return: a pointer to the new string or NULL if str = NULL
+ */
+
+char *_strdup(char *str)
+{
+	char *ptr;
+	unsigned int a, L;
+
+	if (str == NULL)
+		return (NULL);
+
+	for (L = 0; str[L]; L++)
+		;
+
+	ptr = (char *) malloc(L * sizeof(char));
+
+	for (a = 0; a < L; a++)
+	{
+		ptr[a] = str[a];
+	}
+	ptr[a] = '\0';
+
+	return (ptr);
+}
