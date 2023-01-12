4db36a88e293a84761c284fd1ca4fb2e8d7c5897
Justin <tochukwunwanze5@gmail.com>
2023-01-12 03:34:04 -0800

    1-string_nconcat.c, alx project

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -35,7 +35,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	for (i = 0; s1[i]; i++)
 		ptr[i] = s1[i];
 
-	for (j = 0; j < n && s2[j]; j++)
+	for (j = 0; s2[j]; j++)
 	{
 		if (n >= b)
 			ptr[i] = s2[j];
