26e918225776a1a3c80e18798e60f43fda886387
Justin <tochukwunwanze5@gmail.com>
2023-01-18 04:59:38 -0800

    project 3 for function pointers , alx project

--- 0x0F-function_pointers/3-calc.h
+++ 0x0F-function_pointers/3-calc.h
@@ -10,8 +10,8 @@
  */
 typedef struct op
 {
-    char *op;
-    int (*f)(int a, int b);
+	char *op;
+	int (*f)(int a, int b);
 } op_t;
 
 int op_add(int a, int b);
@@ -19,7 +19,6 @@ int op_sub(int a, int b);
 int op_mul(int a, int b);
 int op_div(int a, int b);
 int op_mod(int a, int b);
-int (*get_op_func(char *s))(int, int);
 int (*get_op_func(char *s))(int, int);
 
 #endif /* _CALC_H */
