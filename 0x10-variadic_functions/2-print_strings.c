e1588daf3d0227410b33fd9b5c52876f8b54124f
Justin <tochukwunwanze5@gmail.com>
2023-01-20 19:42:09 -0800

    2-print_strings.c, alx project

---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdarg.h>
+
+/**
+ *print_strings -prints strings, followed by a new line.
+ *@separator: string to be printed between strings
+ *@n: number of strings to be passed into the function
+ *
+ *Return: nothing
+ */
+
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	unsigned int i;
+
+	char *s;
+
+	va_list arg;
+
+	va_start(arg, n);
+
+	for (i = 1; i <= n; i++)
+	{
+		s = va_arg(arg, char *);
+
+		if (s[i] != '\0')
+			printf("%s", s);
+		if (separator && i != n)
+			printf("%s", separator);
+	}
+	printf("\n");
+
+	va_end(arg);
+}
+
