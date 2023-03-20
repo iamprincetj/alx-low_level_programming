6c1bbf36e09e7a4bb0678991f9fcd0166639f8d0
iamprincetj <tochukwunwanze5@gmail.com>
2023-03-20 07:10:10 -0700

    100-operations.so, alx task

---
+++ 0x18-dynamic_libraries/cpython.c
@@ -0,0 +1,64 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+/**
+ * add - addition
+ * @a: number
+ * @b: nuber
+ * Return: result
+ */
+
+int add(int a, int b)
+{
+	return (a + b);
+}
+
+
+/**
+ * sub - subtraction
+ * @a: number
+ * @b: nuber
+ * Return: result
+ */
+
+int sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * mul - multiplication
+ * @a: number
+ * @b: nuber
+ * Return: result
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * divi - division
+ * @a: number
+ * @b: nuber
+ * Return: result
+ */
+
+int divi(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * mod - modulus
+ * @a: number
+ * @b: nuber
+ * Return: result
+ */
+
+int mod(int a, int b)
+{
+	return (a % b);
+}
